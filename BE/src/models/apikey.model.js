61adde7079dc348406e62473d6bf538caf2effa7
giangnt <ntgiang3733@gmail.com>
2024-09-21 02:52:00 +0700

    #4 Check apiKey & permission

---
+++ BE/src/models/apikey.model.js
@@ -0,0 +1,31 @@
+'use strict'
+const { model, Schema } = require('mongoose');
+
+const DOCUMENT_NAME = 'ApiKey';
+const COLLECTION_NAME = 'ApiKeys';
+
+const apiKeySchema = new Schema({
+    key: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    permissions: {
+        type: [String],
+        require: true,
+        enum: ['0000', '1111', '2222']
+    },
+    refreshToken: {
+        type: Array,
+        default: []
+    }
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+
+module.exports = model(DOCUMENT_NAME, apiKeySchema);
